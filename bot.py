448fe3a7cd7c95173eab84109a854a11c6c9f5d3
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2024-05-26 19:07:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,28 +1,24 @@
 import logging
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from pyrogram.handlers import MessageHandler
-from plugins.button_build import ButtonMaker
-from plugins.message_helper import sendMessage
-from urllib.parse import parse_qs, quote, unquote, urlparse, urljoin
+from urllib.parse import unquote, urlparse
 from cloudscraper import create_scraper
-import logging
 import os
-import re
-from os import path
 from time import sleep
 from dotenv import load_dotenv
-from pyrogram import Client
+from plugins.button_build import ButtonMaker
+from plugins.message_helper import sendMessage
 
-API_ID = "your_api_id"
-API_HASH = "your_api_hash"
-BOT_TOKEN = "your_bot_token"  # If you have a bot token
+load_dotenv()
+
+API_ID = os.getenv("API_ID")
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 app = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
-real_debird_api =""
-debird_api=""
-
+DEBRID_LINK_API_TOKEN = os.getenv('DEBRID_LINK_API_TOKEN')
+REAL_DEBRID_API_TOKEN = os.getenv('REAL_DEBRID_API_TOKEN')
 
 debrid_link_sites = ["1dl.net", "1fichier.com", "alterupload.com", "cjoint.net", "desfichiers.com", "dfichiers.com", "megadl.org", 
                 "megadl.fr", "mesfichiers.fr", "mesfichiers.org", "piecejointe.net", "pjointe.com", "tenvoi.com", "dl4free.com", 
@@ -97,40 +93,33 @@ debrid_sites = ['1fichier.com', '2shared.com', '4shared.com', 'alfafile.net', 'a
                 'vivo.sx', 'vk.com', 'voe.sx', 'wdupload.com', 'wipfiles.net', 'world-files.com', 'worldbytez.com', 'wupfile.com',
                 'wushare.com', 'xubster.com', 'youporn.com', 'youtube.com']
 
-
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO) 
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-
-load_dotenv()
-
-
-def direct_link_generator(link):
-    auth =None
-
-    domain = urlparse(link).hostname
-
-    if not domain:
-        raise DirectDownloadLinkException("Error: Url Not Found")
-    elif debird_api and any(x in domain for x in debrid_link_sites):
-        return debrid_link(link)
-    elif real_debird_api and any(x in domain for x in debrid_sites):
-        return real_debrid(link)
-
-
 
 
 class DirectDownloadLinkException(Exception):
     pass
 
+
+def direct_link_generator(link):
+    domain = urlparse(link).hostname
+    if not domain:
+        raise DirectDownloadLinkException("Error: URL Not Found")
+    elif DEBRID_LINK_API_TOKEN and any(x in domain for x in debrid_link_sites):
+        return debrid_link(link)
+    elif REAL_DEBRID_API_TOKEN and any(x in domain for x in debrid_sites):
+        return real_debrid(link)
+    else:
+        raise DirectDownloadLinkException("Error: Unsupported URL")
+
+
 def debrid_link(url):
-    debrid_link_api_token = os.getenv('DEBRID_LINK_API_TOKEN')
-    if not debrid_link_api_token:
+    if not DEBRID_LINK_API_TOKEN:
         raise DirectDownloadLinkException("Debrid-Link API token not provided.")
     
     cget = create_scraper().request
-    resp = cget('POST', f"https://debrid-link.com/api/v2/downloader/add?access_token={debrid_link_api_token}", data={'url': url}).json()
-    if resp['success'] != True:
+    resp = cget('POST', f"https://debrid-link.com/api/v2/downloader/add?access_token={DEBRID_LINK_API_TOKEN}", data={'url': url}).json()
+    if not resp['success']:
         raise DirectDownloadLinkException(f"ERROR: {resp['error']} & ERROR ID: {resp['error_id']}")
     if isinstance(resp['value'], dict):
         return resp['value']['downloadUrl']
@@ -140,7 +129,7 @@ def debrid_link(url):
             if dl.get('expired', False):
                 continue
             item = {
-                "path": path.join(details['title']),
+                "path": os.path.join(details['title']),
                 "filename": dl['name'],
                 "url": dl['downloadUrl']
             }
@@ -149,14 +138,14 @@ def debrid_link(url):
             details['contents'].append(item)
         return details
 
-def real_debrid(url: str, tor=False):
-    real_debrid_api_token = os.getenv('REAL_DEBRID_API_TOKEN')
-    if not real_debrid_api_token:
+
+def real_debrid(url, tor=False):
+    if not REAL_DEBRID_API_TOKEN:
         raise DirectDownloadLinkException("Real-Debrid API token not provided.")
         
     def __unrestrict(url, tor=False):
         cget = create_scraper().request
-        resp = cget('POST', f"https://api.real-debrid.com/rest/1.0/unrestrict/link?auth_token={real_debrid_api_token}", data={'link': url})
+        resp = cget('POST', f"https://api.real-debrid.com/rest/1.0/unrestrict/link?auth_token={REAL_DEBRID_API_TOKEN}", data={'link': url})
         if resp.status_code == 200:
             if tor:
                 _res = resp.json()
@@ -169,22 +158,22 @@ def real_debrid(url: str, tor=False):
     def __addMagnet(magnet):
         cget = create_scraper().request
         hash_ = re.search(r'(?<=xt=urn:btih:)[a-zA-Z0-9]+', magnet).group(0)
-        resp = cget('GET', f"https://api.real-debrid.com/rest/1.0/torrents/instantAvailability/{hash_}?auth_token={real_debrid_api_token}")
+        resp = cget('GET', f"https://api.real-debrid.com/rest/1.0/torrents/instantAvailability/{hash_}?auth_token={REAL_DEBRID_API_TOKEN}")
         if resp.status_code != 200 or len(resp.json()[hash_.lower()]['rd']) == 0:
             return magnet
-        resp = cget('POST', f"https://api.real-debrid.com/rest/1.0/torrents/addMagnet?auth_token={real_debrid_api_token}", data={'magnet': magnet})
+        resp = cget('POST', f"https://api.real-debrid.com/rest/1.0/torrents/addMagnet?auth_token={REAL_DEBRID_API_TOKEN}", data={'magnet': magnet})
         if resp.status_code == 201:
             _id = resp.json()['id']
         else:
             raise DirectDownloadLinkException(f"ERROR: {resp.json()['error']}")
         if _id:
-            _file = cget('POST', f"https://api.real-debrid.com/rest/1.0/torrents/selectFiles/{_id}?auth_token={real_debrid_api_token}", data={'files': 'all'})
+            _file = cget('POST', f"https://api.real-debrid.com/rest/1.0/torrents/selectFiles/{_id}?auth_token={REAL_DEBRID_API_TOKEN}", data={'files': 'all'})
             if _file.status_code != 204:
                 raise DirectDownloadLinkException(f"ERROR: {resp.json()['error']}")
             
         contents = {'links': []}
         while len(contents['links']) == 0:
-            _res = cget('GET', f"https://api.real-debrid.com/rest/1.0/torrents/info/{_id}?auth_token={real_debrid_api_token}")
+            _res = cget('GET', f"https://api.real-debrid.com/rest/1.0/torrents/info/{_id}?auth_token={REAL_DEBRID_API_TOKEN}")
             if _res.status_code == 200:
                 contents = _res.json()
             else:
@@ -196,7 +185,7 @@ def real_debrid(url: str, tor=False):
         for file_info, link in zip(contents['files'], contents['links']):
             link_info = __unrestrict(link, tor=True)
             item = {
-                "path": path.join(details['title'], path.dirname(file_info['path']).lstrip("/")), 
+                "path": os.path.join(details['title'], os.path.dirname(file_info['path']).lstrip("/")), 
                 "filename": unquote(link_info[0]),
                 "url": link_info[1],
             }
@@ -214,8 +203,6 @@ def real_debrid(url: str, tor=False):
         return details['contents'][0]['url']
     return details
 
-# Initialize the Pyrogram Client
-app = Client("my_bot", bot_token=os.getenv('TELEGRAM_BOT_TOKEN'))
 
 @app.on_message(filters.command("rll"))
 async def rll(client: Client, message: Message):
@@ -225,7 +212,7 @@ async def rll(client: Client, message: Message):
             return
 
         url = message.command[1]
-        premium_link = debrid_link(url)
+        premium_link = direct_link_generator(url)
 
         if isinstance(premium_link, str):
             await message.reply_text(f"Here is your premium link: {premium_link}")
@@ -240,6 +227,7 @@ async def rll(client: Client, message: Message):
     except Exception as e:
         logger.error(f"Error occurred: {e}")
         await message.reply_text("An error occurred while processing your request.")
+
 
 @app.on_message(filters.command("rdl"))
 async def rdl(client: Client, message: Message):
@@ -266,13 +254,14 @@ async def rdl(client: Client, message: Message):
         await message.reply_text("An error occurred while processing your request.")
 
 
+@app.on_message(filters.command("start"))
 async def start(client, message):
     start_txt = (
         "Hello, I am <b>Real-Debird Bot</b>,\n"
         "I can Generate <b>Premium Link</b>\n"
         "From Many Sites.\n"
         "To Know What Are The Sites Use <code>/help</code>"
-        "To use me, j<b>oin the update channel using the buttons below.</b>"
+        "To use me, <b>join the update channel using the buttons below.</b>"
     )
     buttons = ButtonMaker()
     buttons.ubutton("Master", "https://t.me/BalaPriyan")
@@ -282,19 +271,20 @@ async def start(client, message):
     await sendMessage(message, start_txt, start_markup)
 
 
+@app.on_message(filters.command("help"))
 async def help(client, message):
-    help_txt= (
+    help_txt = (
         "<code>/rdl</code> {link} \n"
         "To generate premium link "
     )
-    buttons=ButtonMaker
+    buttons = ButtonMaker()
     buttons.ubutton("Master", "https://t.me/BalaPriyan")
     buttons.ubutton("Update", "https://t.me/BalapriyanBots")
     help_markup = buttons.build_menu(2)
 
-    await sendMessage(message,help_txt,help_markup)
+    await sendMessage(message, help_txt, help_markup)
 
 
+if __name__ == "__main__":
+    app.run()
 
-
-app.run()
